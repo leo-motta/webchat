4b0e9d733b29732d2fd29d6c0d5414cebf17bc98
Leonardo Motta <leonardomotta@ucl.br>
2023-01-10 11:09:02 -0300

    feat: dropdown menu

---
+++ client/src/components/DropdownMenu.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { FaBars } from "react-icons/fa"
+import { BsBoxArrowRight, BsGearFill, BsFillChatFill } from "react-icons/bs";
+import { IconContext } from "react-icons";
+
+const DropdownMenu = (props) => {
+    const [open, setOpen] = useState(false)
+    const [profileText, setProfileText] = useState('Profile')
+
+    useEffect(()=> {
+        setOpen(false)
+        if(props.openOptions) {
+            setProfileText('Chat')
+        } else {
+            setProfileText('Profile')
+        }
+        //(props.openOptions) ? setProfileText('Chat') : setProfileText('Profile')
+
+    }, [props.openOptions])
+
+    return (
+        <div>
+            <button onClick={() => setOpen(!open)}>
+                <FaBars className="h-10 p-2 mx-2 mt-8 bg-gray-50 rounded-lg border-2 border-gray-200" size={40} />
+            </button>
+            {open && (
+                <ul className="absolute ml-1 text-[1em] bg-white border-2 border-gray-100 rounded text-sm text-gray-700 dark:text-gray-200">
+                    <li>
+                        <a 
+                            href="#/"
+                            className="flex flex-row  px-4 py-2 pb-2 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                            onClick={() => {props.setOpenOptions(!props.openOptions)}}>
+                        <IconContext.Provider value={{ size: "1.5em", className: " pr-2" }}>
+                            {(props.openOptions) ? <BsFillChatFill /> : <BsGearFill/> }
+                        </IconContext.Provider> {profileText}
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/" className="flex flex-row px-4 py-2 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                        <IconContext.Provider value={{ size: "1.5em", className: " pr-2" }}>
+                            <BsBoxArrowRight />
+                        </IconContext.Provider>Sign out
+                        </a>
+                    </li>
+                </ul>
+            )}
+        </div>
+    )
+}
+
+export default DropdownMenu
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,49 +1,91 @@
 import React from 'react'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { FaBars } from "react-icons/fa"
 import userService from '../features/user/userService'
 import chatService from '../features/chat/chatService'
+import DropdownMenu from './DropdownMenu'
 
 const Sidebar = () => {
     const [searchBox, setSearchBox] = useState('')
     const [isSearching, setIsSearching] = useState(false)
+    const [openOptions, setOpenOptions] = useState(false)
 
-    const { currentUser , userList } = useSelector((state) => state.user)
+    const { currentUser, userList } = useSelector((state) => state.user)
     const { chatList } = useSelector((state) => state.chat)
 
     const dispatch = useDispatch()
 
     //Updates chatList
     useEffect(() => {
-        if(currentUser) {
+        if (currentUser) {
             dispatch(chatService.search(currentUser._id))
         }
-      // eslint-disable-next-line
-    },[isSearching, chatService, currentUser])
-  
+        // eslint-disable-next-line
+    }, [isSearching, chatService, currentUser])
+
     const onKeyDown = (e) => {
-      if (e.code === "Enter") {
-        dispatch(userService.search(searchBox));
-        setIsSearching(true)
-      }
-      if(e.code === "Escape") {
-        setIsSearching(false)
-      }
+        if (e.code === "Enter") {
+            dispatch(userService.search(searchBox));
+            setIsSearching(true)
+        }
+        if (e.code === "Escape") {
+            setIsSearching(false)
+        }
     }
-  
+
     const createChat = (anotherUser) => {
-      if(currentUser) {
-        dispatch(chatService.create({this_userid:currentUser._id, another_userid:anotherUser._id}))
-        setIsSearching(false)
-      }
+        if (currentUser) {
+            dispatch(chatService.create({ this_userid: currentUser._id, another_userid: anotherUser._id }))
+            setIsSearching(false)
+        }
+    }
+
+    const SidebarContent = () => {
+        return (
+            (isSearching && userList) ?
+                userList
+                    .filter((user) => user.name !== currentUser.name)
+                    .map((user) => (
+                        <div className=" w-full flex flex-row cursor-pointer" key={user._id} onClick={() => { createChat(user) }}>
+                            <img className="h-16 w-16 ml-4 my-4 align-center rounded-full" alt="profile" src={user.imageURL} />
+                            <div className="flex flex-col my-4">
+                                <p className="ml-6 text-xl truncate w-36 mb-1">{user.name}</p>
+                                <p className="ml-6 truncate w-36 mb-1">online</p>
+                            </div>
+                        </div>
+                    )
+                    )
+                :
+                (chatList) ?
+                    chatList.map((chat) => {
+                        const user = (chat.firstUser.uid === currentUser._id) ? chat.secondUser : chat.firstUser;
+                        return (
+                            <div className=" w-full flex flex-row cursor-pointer" key={user.uid} onClick={() => { dispatch(chatService.get(chat._id)) }}>
+                                <img className="h-16 w-16 ml-4 my-4 align-center rounded-full" alt="profile" src={user.imageURL} />
+                                <div className="flex flex-col my-4 max-w-2">
+                                    <p className="ml-6 text-xl truncate w-36 mb-1">{user.name}</p>
+                                    <p className="ml-6 truncate w-36 mb-1">{chat.lastMessage}</p>
+                                </div>
+                            </div>
+                        );
+                    })
+                    :
+                    (<p></p>)
+        )
+    }
+
+    const ProfileOptions = () => {
+        return(
+            <>
+                <h1>Profile Options</h1>
+            </>
+        )
     }
 
     return (
         <div className="flex flex-col basis-1/4 border-r-2 border-gray-300">
-
             <div className="bg-white h-24 min-h-24 max-h-24 flex flex-row rounded-tl-lg">
-                <FaBars className="h-10 p-2 my-8 mx-2 bg-gray-50 rounded-lg border-2 border-gray-200" size={40} />
+                <DropdownMenu openOptions={openOptions} setOpenOptions={setOpenOptions} />
                 <input
                     className="w-64 h-10  my-8 p-4 rounded-lg border-2 border-gray-200 text-gray-500 outline-none focus:border-gray-400 transition-colors"
                     placeholder="search for users"
@@ -53,35 +95,9 @@ const Sidebar = () => {
             </div>
 
             <div className="bg-white h-[39em] min-h-[39em] max-h-[39em] overflow-y-scroll">
-                {(isSearching && userList) ?
-                    userList
-                        .filter((user) => user.name !== currentUser.name)
-                        .map((user) => (
-                            <div className=" w-full flex flex-row cursor-pointer" key={user._id} onClick={() => { createChat(user) }}>
-                                <img className="h-16 w-16 ml-4 my-4 align-center rounded-full" alt="profile" src={user.imageURL} />
-                                <div className="flex flex-col my-4">
-                                    <p className="ml-6 text-xl truncate w-36 mb-1">{user.name}</p>
-                                    <p className="ml-6 truncate w-36 mb-1">online</p>
-                                </div>
-                            </div>
-                        )
-                        )
-                    :
-                    (chatList) ?
-                        chatList.map((chat) => {
-                            const user = (chat.firstUser.uid === currentUser._id) ? chat.secondUser : chat.firstUser;
-                            return (
-                                <div className=" w-full flex flex-row cursor-pointer" key={user.uid} onClick={() => { dispatch(chatService.get(chat._id)) }}>
-                                    <img className="h-16 w-16 ml-4 my-4 align-center rounded-full" alt="profile" src={user.imageURL} />
-                                    <div className="flex flex-col my-4 max-w-2">
-                                        <p className="ml-6 text-xl truncate w-36 mb-1">{user.name}</p>
-                                        <p className="ml-6 truncate w-36 mb-1">{chat.lastMessage}</p>
-                                    </div>
-                                </div>
-                            );
-                        })
-                        :
-                        (<p></p>)
+                {
+
+                    (openOptions) ? <ProfileOptions /> : <SidebarContent />
                 }
             </div>
         </div>
